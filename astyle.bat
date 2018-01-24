bcbae7f4f47be4e09d086dc1449f02346c650491
slave in a new era <33710058+gallimaufrystew@users.noreply.github.com>
2018-01-24 16:57:05 +0800

    Add files via upload

---
+++ astyle.bat
@@ -0,0 +1,9 @@
+
+@echo off
+set astyle="C:\AStyle\bin\astyle.exe"
+REM recursively scan directory
+for /r . %%a in (*.cpp;*.c) do %astyle% -A3 -s -xn -xW -Y -p -H -U -k3 -W3 -j -c -xy -xL "%%a"
+for /r . %%a in (*.hpp;*.h) do %astyle% -A3 -s -xn -xW -Y -p -H -U -k3 -W3 -j -c -xy -xL "%%a"
+REM delete all backup file
+for /r . %%a in (*.orig) do del "%%a"
+pause
